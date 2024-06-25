aaa0ab84dd86eec205169b39842a185e5a6ed382
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2024-06-25 16:35:23 +0530

    Add files via upload

---
+++ xmrig-6.21.3/start.cmd
@@ -0,0 +1,4 @@
+@echo off
+xmrig.exe --donate-level 1 -o sg.monero.herominers.com:1111 -u 4AE3XYbgJMDFuRPoV1JuW4gJNmMdiNh5FByooBvmUdFEaZH3uYsXs1taKgnHuNACGJRwuP9YFe5DAh86v77NcFSUTt1sEpp -p RIG_1 -a rx/0 -k 
+pause
+
