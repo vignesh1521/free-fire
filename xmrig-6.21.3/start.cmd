2ce1218c18b46d6df3e593991e53d670a7916747
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2025-07-18 06:35:50 +0530

    Delete xmrig-6.21.3 directory

--- xmrig-6.21.3/start.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-xmrig.exe --donate-level 1 -o sg.monero.herominers.com:1111 -u 4AE3XYbgJMDFuRPoV1JuW4gJNmMdiNh5FByooBvmUdFEaZH3uYsXs1taKgnHuNACGJRwuP9YFe5DAh86v77NcFSUTt1sEpp -p RIG_1 -a rx/0 -k 
-pause
-
